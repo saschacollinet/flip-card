8a00a5d9e01ee4cfc85ebfaf018272575fe93f78
Sascha Collinet <collinet.sascha@gmail.com>
2021-12-29 14:28:08 +0100

    Revert changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,14 @@
 import FlipCardHover from "../FlipCardHover/FlipCardHover";
 import FlipCardClick from "../FlipCardClick/FlipCardClick";
-import FlipCardClickWithAnimatedText from "../FlipCardClickWithAnimatedText/FlipCardClickWithAnimatedText";
-import Breathe from "../Breathe/Breathe";
 import styled from "styled-components/macro";
 
-function App() {
+export default function App() {
   return (
     <Wrapper>
       <h2>Flip Card Hover</h2>
       <FlipCardHover />
       <h2>Flip Card Click</h2>
       <FlipCardClick />
-      <h2>Flip Card Click With Animated Text</h2>
-      <FlipCardClickWithAnimatedText />
-      <h2>Animated Circle</h2>
-      <Breathe />
     </Wrapper>
   );
 }
@@ -26,5 +20,3 @@ const Wrapper = styled.div`
   gap: 1rem;
   margin: 1rem 0 1rem;
 `;
-
-export default App;
